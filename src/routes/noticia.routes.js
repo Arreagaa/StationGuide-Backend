32168a43d8d5b260901cf4b02afff4617bf9a894
Christian Rivas <crivas-2020025@kinal.edu.gt>
2022-07-19 21:33:39 -0600

    Update noticia.routes.js

--- src/routes/noticia.routes.js
+++ src/routes/noticia.routes.js
@@ -8,7 +8,7 @@ const md_roles = require('../middlewares/roles');
 var api = express.Router();
 
 //rutas
-api.get('/obtenerNoticias',[md_autentificacion.Auth],noticiaController.ObtenerNoticias);
+api.get('/obtenerNoticias',noticiaController.ObtenerNoticias);
 api.get('/obtenerNoticiaId/:idNoticia',[md_autentificacion.Auth],noticiaController.ObtenerNoticiaId);
 api.post('/agregarNoticia',[md_autentificacion.Auth, md_roles.verAdmin],noticiaController.agregarNoticia);
 api.put('/editarNoticia/:idNoticia',[md_autentificacion.Auth, md_roles.verAdmin],noticiaController.editarNoticia);
