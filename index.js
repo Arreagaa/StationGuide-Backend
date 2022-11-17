f3b9d8787b4af094588f3bc5a2f9a8882f9ffd48
Christian Rivas <99852554+Arreagaa@users.noreply.github.com>
2022-11-17 16:59:53 -0600

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const app = require('./app');
 mongoose.Promise = global.Promise;                                                                
 mongoose.connect(process.env.STATION_GUIDE, { useNewUrlParser: true, useUnifiedTopology: true }).then(()=>{
     console.log("Se encuentra conectado a la base de datos.");
+    app.get('/', (req,res) => res.send('Helloo'));
 
     app.listen(process.env.PORT|| 3000, function () {
         console.log('Corriendo en el puerto', process.env.PORT)
